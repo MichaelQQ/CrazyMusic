9ba8927c84ff069b33c3747aa2be547135384d98
Ming-Sian Tu <MST-MBPR@Ming-Sians-MacBook-Pro.local>
2015-08-21 16:48:15 +0800

    edit port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express"),
     server = http.createServer(app),
     io = require('socket.io').listen(server);
  
-server.listen(80);
+server.listen(8080);
 console.log('Server running');
  
 app.use('/public', express.static(__dirname + "/public"));
